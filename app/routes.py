260f3233edf7733644a60a3050d4d4968c51551a
Ricardo Fonseca <ricardofonseca95@hotmail.com>
2020-03-19 18:16:09 +0000

    Primeiro commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
 from app import app
+
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/contacto')
+def contacto():
+    return "Email: abcd@gmail.com"
+
+@app.route('/about')
+def about():
+    return "Primeira WebApp criada em 2020"
