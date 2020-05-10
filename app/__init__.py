e5a38e4b69fc48294f2d40d1a98e4aa1b88c57c7
Ricardo Fonseca <ricardofonseca95@hotmail.com>
2020-05-10 20:40:47 +0100

    Primeiro Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_migrate import Migrate
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "218c79e1689b7941e33d339e10280dcf"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,30 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request, session, flash
+from .forms import RegistrationForm,LoginForm
+from .models import User, Post
+
+
+
+posts =[
+    {
+        'autor': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'autor': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 20, 2018'
+    }
+]
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",posts=posts)
 
 @app.route('/contacts')
 def contacts():
@@ -12,4 +32,30 @@ def contacts():
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title ='About')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Failed. Check Nome and Password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+    
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+   form = RegistrationForm()
+   if form.validate_on_submit():
+        flash(f'Account created for {form.nome.data}!', 'success')
+        return redirect(url_for('index'))
+   return render_template('register.html', title='Register', form=form)
+
+@app.route("/logout")
+def logout():
+    session.pop("user",None)
+    return redirect(url_for("login"))
+
